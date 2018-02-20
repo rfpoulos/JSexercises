4ecd37e097da46b01f29221f940386e06822556a
rfpoulos <rfpoulos@outlook.com>
2018-02-20 09:55:44 -0500

    leet function

--- main.js
+++ main.js
@@ -61,3 +61,26 @@ var factorize = function(number) {
 }
 
 console.log(factorize(100));
+
+var leetSpeak = function(sentence) {
+    var leet = {
+        a: '4',
+        e: '3',
+        g: '6',
+        l: '1',
+        o: '0',
+        s: '5',
+        t: '7',
+    };
+    leetSentence = []
+    for (var i = 0; i < sentence.length; i++) {
+        if (leet[sentence[i].toLowerCase()] == undefined) {
+            leetSentence.push(sentence[i]);
+        }
+        else {
+            leetSentence.push(leet[sentence[i].toLowerCase()]);
+        }
+    }
+    return leetSentence.join("");
+}
+console.log(leetSpeak("This is a test"));
