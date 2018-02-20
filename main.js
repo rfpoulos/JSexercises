e4418594913df7705fae67057cc63e3b260b4da7
rfpoulos <rfpoulos@outlook.com>
2018-02-20 10:16:50 -0500

    function sumNumbers

--- main.js
+++ main.js
@@ -97,3 +97,12 @@ var longLongVowels = function(word) {
     return newWord.join('');
 }
 console.log(longLongVowels('book'));
+
+var sumNumbers = function(number) {
+    var total = 0;
+    for (var i = 0; i < number.length; i++) {
+        total += number[i];
+    }
+    return total;
+}
+console.log(sumNumbers([1, 2, 3, 4, 5]));
