4167a1561eff70d8519119d269c86dd8a024f28c
rfpoulos <rfpoulos@outlook.com>
2018-02-20 10:13:10 -0500

    function longLongVowels

--- main.js
+++ main.js
@@ -84,3 +84,16 @@ var leetSpeak = function(sentence) {
     return leetSentence.join("");
 }
 console.log(leetSpeak("This is a test"));
+
+var longLongVowels = function(word) {
+    var vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
+    newWord = [word[0]]
+    for (var i = 1; i < word.length; i++) {
+        if (word[i] == word[i - 1] && vowels[word[i].toLowerCase()] != -1) {
+            newWord.push(word[i] + word[i] + word[i]);
+        }
+        newWord.push(word[i])
+    }
+    return newWord.join('');
+}
+console.log(longLongVowels('book'));
